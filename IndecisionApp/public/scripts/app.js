e5390fb60421db48039308658e54b87044245b8d
John Barrett <johnjbarrett22@gmail.com>
2019-06-09 15:03:45 -0700

    Added Babel to initial app set up

--- IndecisionApp/public/scripts/app.js
+++ IndecisionApp/public/scripts/app.js
@@ -1,8 +1,13 @@
-console.log("App.js is running")
+"use strict";
+
+console.log("App.js is running");
 
 //JSX - JavaScript XML
-// var template = <p>This is JSX from app.js!"</p>
-var template = React.createElement("p", { id: "someid" }, "This is JSX from app.js!");
+var template = React.createElement(
+  "p",
+  null,
+  "Does this change?\""
+);
 var appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
