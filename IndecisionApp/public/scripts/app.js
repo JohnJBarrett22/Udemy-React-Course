c1ce1feaf34951dbe1776ea5cd4146c4707627dd
John Barrett <johnjbarrett22@gmail.com>
2019-06-09 17:07:26 -0700

    Completed assignment

--- IndecisionApp/public/scripts/app.js
+++ IndecisionApp/public/scripts/app.js
@@ -3,11 +3,53 @@
 console.log("App.js is running");
 
 //JSX - JavaScript XML
+var app = {
+    title: 'Indecision App',
+    subtitle: "Put Your Life In The Hands of a Computer"
+
+};
 var template = React.createElement(
-  "p",
-  null,
-  "Does this change?\""
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        app.title
+    ),
+    React.createElement(
+        "p",
+        null,
+        app.subtitle
+    )
 );
+
+var user = {
+    name: 'John',
+    age: 37,
+    location: 'North Hollywood'
+};
+var templateTwo = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        user.name
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Age: ",
+        user.age
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Location: ",
+        user.location
+    )
+);
+
 var appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
--- IndecisionApp/src/app.js
+++ IndecisionApp/src/app.js
@@ -1,7 +1,31 @@
 console.log("App.js is running")
 
 //JSX - JavaScript XML
-var template = <p>Does this change?"</p>
+var app = {
+    title: 'Indecision App',
+    subtitle: "Put Your Life In The Hands of a Computer"
+
+}
+var template = (
+    <div>
+        <h1>{app.title}</h1>
+        <p>{app.subtitle}</p>
+    </div>
+);
+
+var user = {
+    name: 'John',
+    age: 37,
+    location: 'North Hollywood'
+}
+var templateTwo = (
+    <div>
+        <h1>{user.name}</h1>
+        <p>Age: {user.age}</p>
+        <p>Location: {user.location}</p>
+    </div>
+);
+
 var appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
