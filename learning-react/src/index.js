be85315c8e29805ae0c904c7f099c37ae89033fa
John Barrett <johnjbarrett22@gmail.com>
2019-06-11 16:37:20 -0700

    Working on altering state within a stateless function

--- learning-react/src/index.js
+++ learning-react/src/index.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
 const App = () => {
+    const [count, setCount] = useState(10)
+
+    const increment = () => {
+        setCount(count + 1)
+    }
+    const decrement = () => {
+        setCount(count - 1)
+    }
+    const reset = () => {
+        setCount(count = 0)
+    }
+
     return (
         <div>
-            <p>The current count is 0.</p>
+            <p>The current count is {count}.</p>
+            <button onClick={increment}>+1</button>
+            <button onClick={decrement}>-1</button>
+            <button onClick={reset}>Reset</button>
         </div>
     )
 }
