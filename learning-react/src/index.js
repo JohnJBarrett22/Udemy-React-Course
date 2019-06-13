99baa238fc7074b1e4d7ed3a30266e25f781932e
John Barrett <johnjbarrett22@gmail.com>
2019-06-12 21:02:19 -0700

    Finished challenge for a counter, learning about props and use/set state

--- learning-react/src/index.js
+++ learning-react/src/index.js
@@ -2,27 +2,21 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
-const App = () => {
-    const [count, setCount] = useState(10)
-
-    const increment = () => {
-        setCount(count + 1)
-    }
-    const decrement = () => {
-        setCount(count - 1)
-    }
-    const reset = () => {
-        setCount(count = 0)
-    }
+const App = (props) => {
+    const [count, setCount] = useState(props.count)
 
     return (
         <div>
             <p>The current count is {count}.</p>
-            <button onClick={increment}>+1</button>
-            <button onClick={decrement}>-1</button>
-            <button onClick={reset}>Reset</button>
+            <button onClick={() => setCount(count - 1)}>-1</button>
+            <button onClick={() => setCount(props.count)}>Reset</button>
+            <button onClick={() => setCount(count + 1)}>+1</button>
         </div>
     )
+}
+
+App.defaultProps = {
+    count: 0
 }
 
 ReactDOM.render(<App/>, document.getElementById('root'));
