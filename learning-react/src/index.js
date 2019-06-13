fb3fd0331d3e7a6d1f67306b49ed13a4b4047102
John Barrett <johnjbarrett22@gmail.com>
2019-06-12 22:06:32 -0700

    Completed next challenge, though this is not verified in the lesson

--- learning-react/src/index.js
+++ learning-react/src/index.js
@@ -2,23 +2,69 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
-const App = (props) => {
-    const [count, setCount] = useState(props.count)
+const NoteApp = () => {
+    const [notes, setNotes] = useState([])
+    const [title, setTitle] = useState('')
+    const [body, setBody] = useState('')
+
+    const addNote = (e) => {
+        e.preventDefault()
+        setNotes([
+            ...notes,
+            {
+                title,
+                body
+            }
+        ])
+        setTitle('')
+        setBody('')
+
+    }
+
+    const removeNote = (title) => {
+        setNotes(notes.filter((note) => note.title !== title))
+    }
 
     return (
         <div>
-            <p>The current count is {count}.</p>
-            <button onClick={() => setCount(count - 1)}>-1</button>
-            <button onClick={() => setCount(props.count)}>Reset</button>
-            <button onClick={() => setCount(count + 1)}>+1</button>
+            <h1>Notes</h1>
+            {notes.map((note) => (
+               <div key={note.title}>
+                <h3>{note.title}</h3>
+                <h5>{note.body}</h5>
+                <button onClick={() => removeNote(note.title)}>X</button>
+               </div> 
+            ))}
+            <p>Add note</p>
+            <form onSubmit={addNote}>
+                Title: <input value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Title"/>
+                <br></br>
+                Body: <input value={body} onChange={(e) => setBody(e.target.value)} placeholder="Body"/>
+                <button>Add Note</button>
+            </form>
         </div>
     )
 }
 
-App.defaultProps = {
-    count: 0
-}
+// const App = (props) => {
+//     const [count, setCount] = useState(props.count)
+//     const [text, setText] = useState('')
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+//     return (
+//         <div>
+//             <p>The current {text || 'count'} is {count}.</p>
+//             <button onClick={() => setCount(count - 1)}>-1</button>
+//             <button onClick={() => setCount(props.count)}>Reset</button>
+//             <button onClick={() => setCount(count + 1)}>+1</button>
+//             <input value={text} onChange={(e) => setText(e.target.value)}/>
+//         </div>
+//     )
+// }
+
+// App.defaultProps = {
+//     count: 0
+// }
+
+ReactDOM.render(<NoteApp/>, document.getElementById('root'));
 
 serviceWorker.unregister();
